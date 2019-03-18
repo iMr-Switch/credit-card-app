6f305e76524bfe18fdc496fa21d0e07cda35ad60
Tony <tony_oviedo@outlook.com>
2019-03-17 23:01:35 -0500

    Update variable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   clients: boolean = true;
-  advisers: boolean = true;
+  advisers: boolean = false;
   constructor() { }
 
   ngOnInit() {
